0a3309d09f49957467e783ce786415a7084b599c
Chris Pedro <cpedro@quovadis.bm>
2024-10-01 12:50:55 -0300

    Added different function choices for Fibonacci, recursive and iterative.

--- fib.py
+++ fib.py
@@ -15,13 +15,28 @@ import sys
 from signal import signal, SIGINT
 
 
-def fibonacci(n, values={}):
+def fibonacci_recur(n, values={}):
     if n in values:
         return values[n]
-    if n == 0 or n == 1:
+    if n == 0:
+        return 0
+    if n == 1:
         return 1
-    values[n] = fibonacci(n - 1, values) + fibonacci(n - 2, values)
+
+    values[n] = fibonacci_recur(n - 1, values) + fibonacci_recur(n - 2, values)
     return values[n]
+
+
+def fibonacci_iter(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    a, b = 0, 1
+    for i in range(2, n + 1):
+        a, b = b, a + b
+    return b
 
 
 def valid_n(value):
@@ -31,9 +46,18 @@ def valid_n(value):
     return n
 
 
+function_handler = {
+    'recursive': lambda data: fibonacci_recur(data),
+    'iterative': lambda data: fibonacci_iter(data)
+}
+
+
 def parse_args(args):
     parser = argparse.ArgumentParser(description='Fibonacci Calculator')
     parser.add_argument('-n', '--number', dest='n', default='5', type=valid_n)
+    parser.add_argument('-f', '--function', default='iterative',
+                        choices=list(function_handler.keys()),
+                        help='Function to use to calculate')
     return parser.parse_args(args)
 
 
@@ -43,7 +67,10 @@ def handler(signal_received, frame):
 
 def main(args):
     args = parse_args(args)
-    print(f'{fibonacci(args.n)}')
+    if args.function in function_handler:
+        print(function_handler[args.function](args.n))
+    else:
+        raise argparse.ArgumentTypeError('Unsupported function')
 
 
 if __name__ == '__main__':
